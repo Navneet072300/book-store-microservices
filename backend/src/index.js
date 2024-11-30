8fea12dda9156b05e2375a220dc8ee97fda449a5
Navneet Shahi <navneetshahi345@gmail.com>
2024-11-30 17:10:08 +0530

    "Added MongoDB URI to .env file and updated index.js to include new route imports and database connection"

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import aldumsRoutes from "./routes/albums.route.js";
+import songsRoutes from "./routes/songs.route.js";
+import statsRoutes from "./routes/stats.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use("/app/user", userRoutes);
+app.use("/app/auth", authRoutes);
+app.use("/app/admin", adminRoutes);
+app.use("/app/songs", songsRoutes);
+app.use("/app/aldums", aldumsRoutes);
+app.use("/app/stats", statsRoutes);
+
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
+  connectDB();
 });
