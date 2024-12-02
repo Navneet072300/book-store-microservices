1d4d0e69663a1c5fcd56a4afe2f2b3d41a6c36b5
Navneet Shahi <navneetshahi345@gmail.com>
2024-12-02 21:09:58 +0530

    delete 96 files

--- backend/src/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { authCallback } from "../controller/auth.controller.js";
-
-const router = Router();
-
-router.get("/callback", authCallback);
-
-export default router;
